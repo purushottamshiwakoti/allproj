fe26976bbe1556a4b545ddd64a0c1d977a72c6cc
puru12 <purushottam.makura@gmail.com>
2024-03-12 10:50:27 +0545

    removed redux

--- components/get-files.tsx
+++ components/get-files.tsx
@@ -3,9 +3,6 @@
 import { useDispatch, useSelector } from "react-redux";
 
 export const GetFiles = () => {
-  const filesData = useSelector((file: any) => file.storeFiles.users.users);
-  const dispatch = useDispatch();
-  console.log(filesData);
   return (
     <>
       <div>Get files</div>
