90349279ef3fbbf4dc0630554654d29355bd8402
puru12 <purushottam.makura@gmail.com>
2024-03-18 17:42:53 +0545

    fixed csv export

--- actions/csv-download.ts
+++ actions/csv-download.ts
@@ -25,40 +25,7 @@ subFolderId:id
 
 export const downloadQuestions = async (id: any) => {
     // Default values for headers
-    const defaultValues = {
-        SN:"",
-        question: "",
-        questionImage: "",
-        questionAudio: "",
-        questionVideo: "",
-        option: "",
-        optionCorrect: false,
-        optionImage: "",
-        optionAudio: "",
-        optionVideo: "",
-        option1: "",
-        option1Correct: false,
-        option1Image: "",
-        option1Audio: "",
-        option1Video: "",
-        option2: "",
-        option2Correct: false,
-        option2Image: "",
-        option2Audio: "",
-        option2Video: "",
-        option3: "",
-        option3Correct: false,
-        option3Image: "",
-        option3Audio: "",
-        option3Video: "",
-        categoryId: "",
-        subCategoryId: "",
-        typeOfQuestionId: "",
-        projectId: "",
-    };
-
-    // Headers for questions
-    const questionHeaders = Object.keys(defaultValues).join(",");
+  
 
     // Fetch questions from the database
     const data = await db.question.findMany({
@@ -69,14 +36,46 @@ export const downloadQuestions = async (id: any) => {
 
     // Check if any questions were found
     if (data.length === 0) {
-        throw new Error("No questions found for this project ID");
+        throw new Error("No files found");
     }
+    console.log(data)
+    const filteredData=data.map((item,index)=>(
+        {
+            SN:index+1,
+            typeOfQuestionId:item.typeOfQuestionId,
+            categoryId:item.categoryId,
+            subCategoryId:item.subCategoryId,
+            correctOption:item.correctOption,
+            question:item.question,
+            questionImage:item.questionImage,
+            questionAudio:item.questionAudio,
+            questionVideo:item.questionVideo,
+            option:item.option,
+            optionImage:item.optionImage,
+            optionAudio:item.optionAudio,
+            optionVideo:item.optionVideo,
+            option1:item.option1,
+            option1Image:item.option1Image,
+            option1Audio:item.option1Audio,
+            option1Video:item.option1Video,
+            option2:item.option2,
+            option2Image:item.option2Image,
+            option2Audio:item.option2Audio,
+            option2Video:item.option2Video,
+            option3:item.option3,
+            option3Image:item.option3Image,
+            option3Audio:item.option3Audio,
+            option3Video:item.option3Video
+            
+        }
+    ))
 
-    // Convert the data to CSV format with headers
-    const csvContent = [
-        ...data.map((obj) => Object.values(obj).join(","))
-    ].join("\n");
 
-    // Return the CSV content
-    return csvContent;
+const csv = [
+    Object.keys(filteredData[0]).join(","),
+    ...filteredData.map((obj) => Object.values(obj).join(",")),
+  ].join("\n");
+  const csvContent = csv;
+  return csvContent
+ 
 }
