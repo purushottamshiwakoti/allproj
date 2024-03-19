d0accf262430e64c16585e89225ce3fdd18d3fed
puru12 <purushottam.makura@gmail.com>
2024-03-19 20:02:06 +0545

    removed pm2 code

--- app/api/upload-file/[id]/route.ts
+++ app/api/upload-file/[id]/route.ts
@@ -1,9 +1,10 @@
-// Import required modules
 import fs from 'fs';
 import db from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
+
 import { exec } from 'child_process'; // Import exec for executing shell commands
+
 
 export async function POST(req: NextRequest, params: any) {
   try {
@@ -13,14 +14,12 @@ export async function POST(req: NextRequest, params: any) {
         id: params.params.id
       }
     });
-
     if (!project) {
-      return NextResponse.json({ message: "Project not found" }, { status: 404 });
+      return NextResponse.json({
+        message: "Project not found"
+      }, { status: 404 });
     }
-
     const projectFolder = project.slug;
-    const commandsToExecute = [];
-
     for (const [name, file] of data.entries()) {
       if (file && typeof file === "object") {
         const byteData = await file.arrayBuffer();
@@ -28,7 +27,8 @@ export async function POST(req: NextRequest, params: any) {
         let fileName = file.name;
         let fileNumber = 1;
         let filePath = path.join(process.cwd(), "public", projectFolder, fileName);
-
+        
+        // Check if the file path already exists
         while (fs.existsSync(filePath)) {
           const extensionIndex = fileName.lastIndexOf(".");
           const extension = fileName.substring(extensionIndex);
@@ -37,27 +37,26 @@ export async function POST(req: NextRequest, params: any) {
           fileNumber++;
           filePath = path.join(process.cwd(), "public", projectFolder, fileName);
         }
-
-        const relativePath = path.relative(path.join(process.cwd(), "public"), filePath);
+        
+       const relativePath = path.relative(path.join(process.cwd(), "public"), filePath);
+        
+        // Write the file to disk
         fs.writeFileSync(filePath, buffer);
-        commandsToExecute.push(`pm2 restart myapp`);
+      //   exec("pm2 restart myapp", (error, stdout, stderr) => {
+      //     if (error || stderr) {
+      //         const errorMessage = error ? error.message : '';
+      //         const stderrMessage = stderr ? stderr.toString() : '';
+      //         console.log(`Error executing pm2 restart myapp: ${errorMessage}\n${stderrMessage}`);
+      //         // return NextResponse.json({ message: "Error restarting myapp", error: errorMessage, stderr: stderrMessage }, { status: 500 });
+      //     }
+      //     console.log(`pm2 restart myapp output: ${stdout}`);
+      // });
+      
+        return NextResponse.json({ relativePath }, { status: 200 });
       }
     }
-
-    // Execute the PM2 restart command once all files have been processed
-    exec(commandsToExecute.join(" && "), (error, stdout, stderr) => {
-      if (error || stderr) {
-        const errorMessage = error ? error.message : '';
-        const stderrMessage = stderr ? stderr.toString() : '';
-        console.log(`Error executing pm2 restart myapp: ${errorMessage}\n${stderrMessage}`);
-      }
-      console.log(`pm2 restart myapp output: ${stdout}`);
-    });
-
-    return NextResponse.json({ message: "Files uploaded successfully" }, { status: 200 });
-
   } catch (error) {
-    console.log("Error:", error);
-    return NextResponse.json({ message: "Something went wrong while uploading files" }, { status: 500 });
+    console.log("error is", error);
+    return NextResponse.json({ message: "Something went wrong while uploading file" }, { status: 500 });
   }
 }
